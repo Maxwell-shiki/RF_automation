4c1a249d1cdd325dbb01245d2fcdcbff67d1b395
shiki <maxwellxcz@gmail.com>
2023-10-24 12:39:36 +0800

    add SigGen func set_freq, set_power, get_freq and get_power

---
+++ Signal_generator_14565L/test/SigGen_1465L.py
@@ -0,0 +1,76 @@
+import pyvisa as visa
+import numpy as np
+import time
+
+class Demo():
+    def __init__(self, gpib, *args):
+        self.gpib = gpib
+        self.addr = 'GPIB0::%s::INSTR' % self.gpib
+        self.rm = visa.ResourceManager()
+    
+    def open(self):
+        self.inst = self.rm.open_resource(self.addr)
+        self.inst.read_termination = '\n'
+        # self.inst.write('*RST\n')
+    
+    def close(self):
+        if self.inst is not None:
+            self.inst.close()
+            self.inst = None
+    
+    def set_freq(self, freq, step=None, offset=None, ref=None, mult=None):
+        self.inst.write(':FREQ %s\n' % freq)
+        if step is not None:
+            self.inst.write(':FREQ:STEP %s\n' % step)
+        if offset is not None:
+            self.inst.write(':FREQ:OFFS %s\n' % offset)
+        if ref is not None:
+            self.inst.write(':FREQ:REF %s\n' % ref)
+        if mult is not None:
+            self.inst.write(':FREQ:MULT %s\n' % mult)
+    
+    def set_power(self, power, offset=None, ref=None):
+        self.inst.write(':POW %s\n' % power)
+        if offset is not None:
+            self.inst.write(':POW:OFFS %s\n' % offset)
+        if ref is not None:
+            self.inst.write(':POW:REF:STAT 1\n')
+            self.inst.write(':POW:REF %s\n' % ref)
+
+    def get_freq(self):
+        freq = self.inst.query('FREQ?')
+        # step = self.inst.query('FREQ:STEP?')
+        # offset = self.inst.query('FREQ:OFFS?')
+        print('频率为:     \t', freq, ' Hz')
+        # print('频率步进为: \t', step, ' Hz')
+        # print('频率偏移为: \t', offset, ' Hz')
+        print()
+    
+    def get_power(self):
+        power = self.inst.query('POW?')
+        # offset = self.inst.query('POW:OFFS?')
+        # ref = self.inst.query('POW:REF?')
+        print('功率为:     \t', power, ' dBm')
+        # print('功率偏置为: \t', offset, ' dB')
+        # print('功率参考为: \t', ref, ' dBm')
+        print()
+
+
+
+if __name__ == "__main__":
+    de = Demo(19)
+    de.open()
+
+    de.set_freq('300MHz')
+    # de.set_freq('300MHz', '0MHz')
+    # de.set_freq('500MHz', '10MHz', '0GHz', None, '5')
+
+    de.set_power('20dBm')
+
+    
+
+    # de.get_freq()
+    # de.get_power()
+
+
+    de.close()
