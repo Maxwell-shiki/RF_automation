f0ad234b012d6eb198eb1e1affdd6ca47f3988e0
shiki <maxwellxcz@gmail.com>
2024-02-20 18:24:51 +0800

    feat(TEST/4_COMassistant):增加Python编写的串口通信基础脚本

---
+++ TEST/4_COMassistant/others/Pyserial/readme.md
@@ -0,0 +1,9 @@
+Python 串口通信
+-----
+- Pyserial
+    - 参考：[用 Python 玩转串口（基于 pySerial）](https://blog.csdn.net/bryanwang_3099/article/details/120493736)
+    - [Pyserial 介绍(知乎)](https://zhuanlan.zhihu.com/p/676933536)
+    - 此处 波特率9600，检验位无，停止位1
+
+
+- 如果有UI要求，参考 [tkinter+pyserial实现串口调试助手](https://blog.csdn.net/dolphintt920/article/details/128154593)
---
+++ TEST/4_COMassistant/others/Pyserial/script.py
@@ -0,0 +1,75 @@
+# 串口的基本操作
+# 1. 确定串口号
+# 2. 配置波特率、数据位、奇偶校验位、停止位、DTR/DSR、RTS/CTS、XON/XOFF
+# 3. 打开串口
+# 4. 收发数据
+# 5. 关闭串口
+
+import sys, os
+import time
+import serial
+import serial.tools.list_ports
+
+def print_portslist():
+    ports_list = list(serial.tools.list_ports.comports())
+    if len(ports_list) == 0:
+        print("没有发现串口设备")
+    else:
+        print("发现以下串口设备：")
+        for comport in ports_list:
+            print(list(comport)[0], list(comport)[1])
+
+def write(filename, ser):
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            ser.write(line.encode("utf-8"))
+            time.sleep(0.1)
+
+# 将读取到的数据写入文件
+def read(filename, ser):
+    with open(filename, 'w') as f:
+        data = ser.read_all()
+        f.write(data)
+
+if __name__ == "__main__":
+    print_portslist()
+    
+    # 打开并配置串口
+    ser = serial.Serial(port="COM18",                       # 串口号
+                        baudrate=9600,                      # 波特率
+                        # bytesize=5,                       # 数据位
+                        parity=PARITY_NONE,                 # 校验位
+                        stopbits=STOPBITS_ONE               # 停止位
+                        # timeout=None,                     # 读超时
+                        # write_timeout=0                   # 写超时
+    )
+    
+    # if ser.isOpen():
+    #     print("打开串口成功")
+    # else:
+    #     print("打开串口失败")
+    
+    # 发送数据
+    command = ser.write('hello'.encode("utf-8"))
+    # command = ser.write(b'hello')
+    
+    # filedir = "./data/"
+    # srcfile = "send.txt"
+    # write(filedir + filename, ser)
+    # destfile = "received.txt"
+    # read(destfile, ser)
+    
+    time.sleep(10)
+    
+    # 接收数据
+    # ser.readline()      # 读取一行
+    # ser.read_all()      # 读取所有数据
+    data = ser.read(10)        # 读取10个字节
+    print("接收到的数据：", data)
+
+    # 关闭串口
+    ser.close()
+    if ser.isOpen():
+        print("关闭串口失败")
+    else:
+        print("串口已关闭")
