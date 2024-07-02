72f394a0e9911136e8360cae977f6cc771410df4
shiki <maxwellxcz@gmail.com>
2024-07-02 15:02:40 +0800

    update

--- TEST/0_LDO/test.py
+++ TEST/0_LDO/test.py
@@ -21,26 +21,35 @@ for root, dirs, files in os.walk(path_scripts):
         sys.path.append(path_script)
 
 from DCPowerSupply_ES3631A import DCPowerSupply_ES3631A  
-from Multimeter_3458A import Multimeter_3458A
 from SignalGenerator_1465L import SignalGenerator_1465L
-from MicrowavePowerMeter_2438PA import MicrowavePowerMeter_2438PA
-from VectorNetworkAnalyzer_3672E import VectorNetworkAnalyzer_3672E
-from DCElectronicLoad_ET5300A import DCElectronicLoad_ET5300A
+# from DCElectronicLoad_ET5300A import DCElectronicLoad_ET5300A
+from Oscilloscope_MSO64B import Oscilloscope_MSO64B
 
 
 def main():
-    comload = DCElectronicLoad_ET5300A('COM3')
 
-    # comload.constCurrent(1.25)
-    # comload.constVoltage(9.2)
-    # comload.constCur2Vol(0.82, 7.2)
+    # PS_resource_name = "USB0::0x2A8D::0x1002::MY61003060::0::INSTR"
+    # DCPS = DCPowerSupply_ES3631A(PS_resource_name)
+    # # DCPS.set_voltage(3.1, channel = 1)
+
+
+    # comload = DCElectronicLoad_ET5300A('COM8')
+    # # comload.constCurrent(0.2)
+    # # comload.constVoltage(9.2)
+    # # comload.constCur2Vol(0.82, 7.2)
     # comload.applyload('ON')
 
-    Imax = comload.query('CURR:IMAX?')
-    print(Imax)
-    comload.write('CURR:IMAX 1.81')
-    Imax = comload.query('CURR:IMAX?')
-    print(Imax)
+    # # SG_resource_name = "GPIB0::19::INSTR"
+
+    # # MSO_resource_name = "USB0::0x0699::0x0401::C011422::0::INSTR"
+
+    # **********************************************************************
+
+    
+
+
+
+
 
 
 
